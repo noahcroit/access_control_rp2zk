315cb9e17993de7f4ede6104befbfb90c1dad450
noah <nattapong.w1004@gmail.com>
2024-02-03 22:10:57 +0700

    rp2, add more comment on how to use scheduler

--- rp2/q_scheduler.py
+++ rp2/q_scheduler.py
@@ -1,7 +1,42 @@
 import heapq
 
 
+""" Task Scheduler with Function-Queue method
+    First, I created this for my micropython project. It can be used in python too but there are better options.
 
+    FunctionQueueTask      -> Class for create Task
+    FunctionQueueScheduler -> Class for Task Scheduler
+
+    How to use:
+        1) Create scheduler first by using FunctionQueueScheduler class
+
+            s = FunctionQueueScheduler()
+
+        2) Create task(s) by using FunctionQueueTask class
+
+            t = FunctionQueueTask(<task function>, <task ID>, s)
+            <task function> : function of that task to run whenever scheduler called the task
+            <task ID> : task number. scheduler will use Min-Heap as function queue,
+                        to select which task is needed to be run first.
+                        So, Least ID number means most Task Priority
+            s : scheduler from 1)
+
+        3) Install task(s) from 2) into scheduler by
+
+            s.installTask(t1)   # task 1
+            s.installTask(t2)   # task 2
+            s.installTask(t3)   # task 3
+                .
+                .
+                .
+
+        4) To put task in scheduler's queue, Call s.pushTask(<task ID>)
+            Or task can be put automatically by binding t.handler to the callback function of HW interrupt
+            (Timer, GPIO etc.)
+
+        5) Call s.popTask() in main loop. If there are task(s) in the queue, It will be run immediately.
+
+"""
 class FunctionQueueTask:
     def __init__(self, task_fn, task_id, scheduler=None):
         self.task_fn = task_fn
@@ -18,7 +53,6 @@ class FunctionQueueTask:
 
 class FunctionQueueScheduler:
     def __init__(self):
-        # heap q?
         self.fq = []
         heapq.heapify(self.fq)
         self.task_dict = {}
