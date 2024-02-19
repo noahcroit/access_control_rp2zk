619104259d29448ba7bfa36a6811219eb3b0b9b2
noah <nattapong.w1004@gmail.com>
2024-02-20 05:39:28 +0700

    wworker, update access_control class

--- worker/access_control.py
+++ worker/access_control.py
@@ -4,9 +4,10 @@ import aiomqtt
 import aioredis
 import queue
 import json
-import datetime
+import time
 from zk import ZK
 from zk import const as zk_const
+from datetime import datetime
 
 
 
@@ -129,8 +130,6 @@ class FailSecureLock(SecureLock):
 
 
 
-
-
 class ZktecoLock(SecureLock):
     def __init__(self, name, cfg, roomtype):
         super().__init__(name, cfg)
@@ -160,13 +159,18 @@ class ZktecoLock(SecureLock):
     def disable(self):
         self.conn.disable_device()
 
+    def synctime(self):
+        self.conn.set_time(datetime.now())
+
     def getUsers(self):
         users = self.conn.get_users()
         return users
 
     def displayInfoUsers(self):
+        d_users = {}
         users = self.getUsers()
         for user in users:
+            d_user = {}
             privilege = 'User'
             if user.privilege == zk_const.USER_ADMIN:
                 privilege = 'Admin'
@@ -176,35 +180,100 @@ class ZktecoLock(SecureLock):
             print('  Password   : {}'.format(user.password))
             print('  Group ID   : {}'.format(user.group_id))
             print('  User  ID   : {}'.format(user.user_id))
+            d_user.update({"username":user.name})
+            d_user.update({"password":user.password})
+            d_user.update({"privilege":privilege})
+            d_users.update({user.user_id:d_user})
+        return d_users
+
+    def addUser(self, name, userid, pwd, userlevel):
+        self.conn.set_user(name=name, privilege=userlevel, password=pwd, user_id=userid)
+
+    def delUser(self, userid=None, name=""):
+        if userid:
+            self.conn.delete_user(user_id=userid)
+
+    def generate_otp(self):
+        import random
+        otp = "".join([random.choice("0123456789") for _ in range(4)])
+        return otp
 
     def isDoorLock(self):
         pass
 
-    async def unlock(self, duration_min=1):
+    async def unlock_delay(self, duration_sec=10):
         if not self.unlocking:
-            self.disable()
-            self.conn.unlock(duration_min * 6)
             self.unlocking = True
-            self.enable()
-            duration_sec = duration_min * 60
-            t_start = datetime.datetime.now(datetime.timezone.utc)
+            t_start = int(time.time())
             t_diff = 0
-            while t_diff < duration_sec:
+            while t_diff < duration_sec and self.unlocking:
+                self.disable()
+                self.conn.unlock()
+                self.unlocking = True
+                self.enable()
                 # calculate diff time
-                diff = datetime.datetime.now(datetime.timezone.utc) - t_start
-                t_diff = int(diff.total_seconds())
-                await asyncio.sleep(5)
+                await asyncio.sleep(3)
+                t_diff = int(time.time()) - t_start
             self.unlocking = False
+
+    def lock_soft(self):
+        self.unlocking = False
+
+    async def room_reserve(self, epox_start, epox_end, room_pwd="1234"):
+        print("Unlock start at ", epox_start, ", end at", epox_end)
+        # read epox time
+        diff = epox_end - epox_start
+        time_current = int(time.time())
+        # do nothing until epox_start
+        while time_current < epox_start:
+            await asyncio.sleep(3)
+            time_current = int(time.time())
+        # create temporary user for attendance within period of time, and delete user
+        print("Create temporary user for ", diff, "sec")
+        self.addUser(name="reserve", userid="100", pwd=room_pwd, userlevel=zk_const.USER_DEFAULT)
+        # reserve method
+        # "always unlock" or "only add user"
+        #await self.unlock_delay(duration_sec=diff)
+        await asyncio.sleep(diff)
+        self.delUser(userid="100")
+
+    def redisJsonLoad(self, json_string, func="ls"):
+        data = json.loads(json_string)
+        if func == "ls":
+            pass
+        elif func == "add":
+            uid  = data["id"]
+            user = data["username"]
+            pwd  = data["password"]
+            return uid, user, pwd
+        elif func == "del":
+            uid  = data["id"]
+            user = data["username"]
+            return uid, user
+        elif func == "reserve":
+            epox_start = int(data["epox_start"])
+            epox_end = int(data["epox_end"])
+            return epox_start, epox_end
+
+    async def listenAtten(self):
+        while True:
+            print("Listen to atten event...")
+            await asyncio.sleep(10)
 
     async def listenRedis(self):
         # Tag list for REDIS sub
-        tagname_lock = self.cfg['device_tags']['profacex']['lockctrl']
+        tagname_reserve = self.cfg['device_tags']['profacex']['reserve_request']
         tagname_add = self.cfg['device_tags']['profacex']['adduser']
         tagname_del = self.cfg['device_tags']['profacex']['deluser']
+        tagname_userinfo = self.cfg['device_tags']['profacex']['userinfo']
+        tagname_response = self.cfg['device_tags']['profacex']['response']
+        tagname_sync = self.cfg['device_tags']['profacex']['synctime']
         # REDIS subscribe
-        await self.rpubsub.subscribe('settag:'+tagname_lock)
+        await self.rpubsub.subscribe('settag:'+tagname_reserve)
         await self.rpubsub.subscribe('settag:'+tagname_add)
         await self.rpubsub.subscribe('settag:'+tagname_del)
+        await self.rpubsub.subscribe('settag:'+tagname_userinfo)
+        await self.rpubsub.subscribe('settag:'+tagname_sync)
         while True:
             # wait redis message
             msg = await self.rpubsub.get_message(ignore_subscribe_messages=True)
@@ -218,12 +287,42 @@ class ZktecoLock(SecureLock):
                 val = msg['data'].decode('utf-8')
 
                 # check tagname
-                if ch == tagname_lock:
-                    await self.unlock(duration_min=int(val))
+                if ch == tagname_reserve:
+                    # extract message info
+                    epox_start, epox_end = self.redisJsonLoad(func="reserve", json_string=val)
+                    # generate OTP as password for room reserver & publish as ACK
+                    otp = self.generate_otp()
+                    d_otp = {"reserve" : {"id": "100", "otp": otp}}
+                    json_message = json.dumps(d_otp)
+                    await self.redis.publish("tag:"+tagname_response, json_message)
+                    # make room reservation
+                    asyncio.create_task(self.room_reserve(epox_start, epox_end, otp))
+
+                elif ch == tagname_add:
+                    # extract message info
+                    userid, username, pwd = self.redisJsonLoad(func="add", json_string=val)
+                    # add user
+                    self.addUser(name=username, userid=userid, pwd=pwd, userlevel=zk_const.USER_DEFAULT)
+
+                elif ch == tagname_del:
+                    # extract message info
+                    userid, username = self.redisJsonLoad(func="del", json_string=val)
+                    self.delUser(userid=userid)
+
+                elif ch == tagname_userinfo:
+                    d_users = self.displayInfoUsers()
+                    json_message = json.dumps({"users":d_users})
+                    await self.redis.publish("tag:"+tagname_response, json_message)
+                    print("userinfo sent")
+
+                elif ch == tagname_sync:
+                    self.synctime()
+
             await asyncio.sleep(1)
 
     async def run(self):
         if self.conn:
             print("ZKteco:" + self.name + ", start running")
             asyncio.create_task(self.listenRedis())
+            asyncio.create_task(self.listenAtten())
 
