53656f94d9804e63f2755b7345f4d1eb6e8d0cac
noah <nattapong.w1004@gmail.com>
2025-03-11 06:18:05 +0700

    firmware, add app button

---
+++ v2024/firmware/include/app_button.h
@@ -0,0 +1,27 @@
+#ifndef APP_BUTTON_H
+#define APP_BUTTON_H
+
+#include "board_driver_rp2.h"
+
+typedef struct {
+    bool (*driver_read_gpio) (uint8_t);
+    void (*driver_set_gpio_input) (uint8_t);
+    void (*driver_enable_gpio_global_interrupt) (void);
+    void (*driver_set_gpio_callback) (uint8_t, void (*) (void));
+    void (*cb_onboard) (void);
+    void (*cb_exit) (void);
+    uint8_t gpio_num_onboard_button;
+    uint8_t gpio_num_exit_button;
+    uint8_t gpio_num_dipsw1;
+    uint8_t gpio_num_dipsw2;
+    uint8_t gpio_num_dipsw3;
+    uint8_t gpio_num_dipsw4;
+
+}app_button_t;
+
+void app_button_init(app_button_t *dev);
+bool app_is_button_pressed_onboard(app_button_t *dev);
+bool app_is_button_pressed_exit(app_button_t *dev);
+uint8_t app_read_dipswitch(app_button_t *dev);
+
+#endif
---
+++ v2024/firmware/src/app_button.c
@@ -0,0 +1,46 @@
+#include "app_button.h"
+
+
+
+void app_button_init(app_button_t *dev) {
+    // GPIO basic input setup
+    dev->driver_set_gpio_input(dev->gpio_num_onboard_button);
+    dev->driver_set_gpio_input(dev->gpio_num_exit_button);
+    dev->driver_set_gpio_input(dev->gpio_num_dipsw1);
+    dev->driver_set_gpio_input(dev->gpio_num_dipsw2);
+    dev->driver_set_gpio_input(dev->gpio_num_dipsw3);
+    dev->driver_set_gpio_input(dev->gpio_num_dipsw4);
+    
+    // Interrupt setup
+    dev->driver_enable_gpio_global_interrupt();
+    dev->driver_set_gpio_callback(dev->gpio_num_onboard_button, dev->cb_onboard);
+    dev->driver_set_gpio_callback(dev->gpio_num_exit_button, dev->cb_exit);
+}
+
+bool app_is_button_pressed_onboard(app_button_t *dev) {
+    return !(dev->driver_read_gpio(dev->gpio_num_onboard_button));
+}
+
+bool app_is_button_pressed_exit(app_button_t *dev) {
+    return !(dev->driver_read_gpio(dev->gpio_num_exit_button));
+}
+
+uint8_t app_read_dipswitch(app_button_t *dev) {
+    uint8_t sw[4];
+    sw[0] = dev->driver_read_gpio(dev->gpio_num_dipsw1);
+    sw[1] = dev->driver_read_gpio(dev->gpio_num_dipsw2);
+    sw[2] = dev->driver_read_gpio(dev->gpio_num_dipsw3);
+    sw[3] = dev->driver_read_gpio(dev->gpio_num_dipsw4);
+    return (sw[3] << 3) | (sw[2] << 2) | (sw[1] << 1) | (sw[0] << 0);
+}
+
+/*
+void on_button_pressed_onboard(void *app_data) {
+
+}
+
+void on_button_pressed_exit(void *app_data) {
+
+}
+*/
+
