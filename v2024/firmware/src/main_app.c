8bbfcfe0102f855b5b27b5ee5772e4da43cdc0fa
noah <nattapong.w1004@gmail.com>
2025-03-12 17:05:25 +0700

    firmware, update main

--- v2024/firmware/src/main_app.c
+++ v2024/firmware/src/main_app.c
@@ -4,6 +4,8 @@
 #include "app_led.h"
 #include "app_buzzer.h"
 #include "app_dsk4t100.h"
+#include "app_outage.h"
+#include "app_accessctrl.h"
 
 #define DI_ZEROCROSS 2
 #define DI_USERBUTTON 10
@@ -18,6 +20,7 @@
 #define DO_DOORLOCK 18
 #define DI_DOORSTATUS 19
 #define DI_LOCKSTATUS 20
+#define DO_SLIDEDOOR 28
 
 
 
@@ -70,20 +73,44 @@ app_dsk4t100_t locker = {
     .gpio_num_lockctrl = DO_DOORLOCK,
     .gpio_num_lockstatus = DI_LOCKSTATUS,
     .gpio_num_doorstatus = DI_DOORSTATUS
-};  
+};
+
+app_outage_t outage = {
+    .driver_read_gpio = driver_rp2_read_gpio,
+    .driver_set_gpio_input = driver_rp2_set_gpio_input,
+    .driver_enable_gpio_global_interrupt = driver_rp2_enable_gpio_global_interrupt,
+    .driver_set_gpio_callback = driver_rp2_set_gpio_callback,
+    .gpio_num = DI_ZEROCROSS,
+    .threshold = 5
+};
+
+app_accessctrl_t ac = {
+    .driver_write_gpio = driver_rp2_write_gpio,
+    .driver_set_gpio_output = driver_rp2_set_gpio_output,
+    .gpio_num_slidedoor = DO_DOORLOCK,
+    .lockstate = false
+};
 
 
 
+/*
+ * main loop
+ *
+ */
 int main() {
     // system initalize
     driver_rp2_sysinit();
+    driver_rp2_create_global_tick(1000);
+
     // App initialize
     app_button_init(&button);
     app_led_init(&led);
     app_buzzer_init(&buzzer);
     app_dsk4t100_init(&locker);
+    app_accessctrl_init(&ac);
+    app_outage_init(&outage);
 
-    while (1) {
+    while (1){ 
         // button function for testing LEDs & bolt-lock
         if (app_is_button_pressed_onboard(&button)) {
             app_led_y_on(&led);
@@ -97,14 +124,27 @@ int main() {
         }
 
         // check door status from bolt-lock
+        /*
         bool isopen;
         isopen = app_dsk4t100_is_open(&locker);
         driver_debug_print("door status : ");
         driver_debug_print_int(isopen);
         driver_debug_print("\n");
-        
+        */
+
+        // check outage
+        bool isoutage;
+        isoutage = app_is_outage_occured(&outage);
+        driver_debug_print("outage status : ");
+        driver_debug_print_int(isoutage);
+        driver_debug_print("\n");
+
         // sleep
-        driver_sleep_ms(250);
+        uint32_t tick = driver_rp2_get_global_tick();
+        driver_debug_print("global tick=");
+        driver_debug_print_int(tick);
+        driver_debug_print("\n");
+        driver_sleep_ms(1000);
     }
     return 0;
 }
